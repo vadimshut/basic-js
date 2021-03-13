d1cc8cb756240a680bf7b6d6b6fdd0b65feed363
vadimshut <vadimshut1992@gmail.com>
2021-03-14 00:50:42 +0300

    feat: implement dreamTeam function

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
+  if (matrix == undefined){
+    throw new CustomError('Not implemented');
+  }
   let arr = matrix.reduce( (acc, item) => {
       return acc.concat(item);
   }, []);
   let catsCount = arr.filter( item => item == "^^").length;
   return catsCount;
 };
+//Passing: 20. Failing: 0. Pending: 52  1 task
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  //throw new CustomError('Not implemented');
+  if (!Array.isArray(members)){
+    return false
+  }
+  let stringMembers = members.filter( item => typeof(item) == 'string');
+  let firstLetters = stringMembers.map( item => item.trim()[0].toUpperCase());
+  let result = firstLetters.sort().join('');
+  return result
 };
+// 28 passing 44 pending  2 task
