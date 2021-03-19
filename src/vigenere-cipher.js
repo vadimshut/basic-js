4f6a898add3538494134fcaed2cff8f340d6b34d
vadimshut <vadimshut1992@gmail.com>
2021-03-19 15:31:38 +0300

    feat: implement function vigenera-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(a=true){
+      this.a = a;
+      this.alphaArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+  }
+  encrypt(message, key) {  
+      if (message === undefined || key === undefined) throw new Error();
+      let alphaLength = this.alphaArray.length;
+      let keyArr = key.toUpperCase().split('');
+      let keyIndexArr = 0;
+      let result = message.toUpperCase().split('').map(item => {
+          if (this.alphaArray.includes(item)){
+              let keyIndex = this.alphaArray.indexOf(keyArr[keyIndexArr]);           
+              let mesgEnscriptIndex = (keyIndex + this.alphaArray.indexOf(item)) % alphaLength;
+              keyIndexArr = ++keyIndexArr % keyArr.length;           
+              return this.alphaArray[mesgEnscriptIndex];
+          }
+          return item;
+      });
+      return this.a ? result.join('') : result.reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+      if (encryptedMessage === undefined || key === undefined) throw new Error();
+      let alphaLength = this.alphaArray.length;
+      let keyArr = key.toUpperCase().split('');
+      let keyIndexArr = 0;
+      let result = encryptedMessage.toUpperCase().split('').map(item => {
+          if (this.alphaArray.includes(item)) {
+              let keyIndex = this.alphaArray.indexOf(keyArr[keyIndexArr]);           
+              let mesgEnscriptIndex = (this.alphaArray.indexOf(item) - keyIndex);
+              if(mesgEnscriptIndex < 0) mesgEnscriptIndex += alphaLength
+              keyIndexArr = ++keyIndexArr % keyArr.length;           
+              return this.alphaArray[mesgEnscriptIndex];
+          }
+          return item;
+      });
+      return this.a ? result.join('') : result.reverse().join('');
   }
 }
 
